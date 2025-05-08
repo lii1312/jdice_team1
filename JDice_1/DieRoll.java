d830834aa932c7887e6cc5347f66086b553934ff
lili <huynhthily.021225@gmail.com>
2025-05-08 11:10:59 +0700

    Add GitHub Actions workflow for testing

--- JDice_1/DieRoll.java
+++ JDice_1/DieRoll.java
@@ -10,6 +10,7 @@ import java.util.logging.Logger;
  * - Added input validation in constructor.
  * - Added logging for each die roll.
  */
+<<<<<<< HEAD
 public class DieRoll {
     private final int numberOfDice;
     private final int numberOfSides;
@@ -64,5 +65,42 @@ public class DieRoll {
         if (bonus > 0) builder.append("+").append(bonus);
         else if (bonus < 0) builder.append(bonus);
         return builder.toString();
+=======
+
+
+ public class DieRoll {
+    private int ndice;
+    private int nsides;
+    private int bonus;
+    private static Random rnd;
+
+    static {
+        rnd = new Random();
+    }
+
+    public DieRoll(int ndice, int nsides, int bonus) {
+        this.ndice = ndice;
+        this.nsides = nsides;
+        this.bonus = bonus;
+    }
+
+    public RollResult makeRoll() {
+        RollResult r = new RollResult(bonus);
+        for (int i = 0; i < ndice; i++) {
+            int roll = rnd.nextInt(nsides) + 1; // fix: thêm +1 để ra số từ 1
+            r.addResult(roll);
+        }
+        return r;
+    }
+
+    public String toString() {
+        String ans = ndice + "d" + nsides;
+        if (bonus > 0) {
+            ans = ans + "+" + bonus;
+        } else if (bonus < 0) {
+            ans = ans + bonus;
+        }
+        return ans;
+>>>>>>> 4c3f7b3 (Fix syntax and logic errors in DieRoll class)
     }
 }
